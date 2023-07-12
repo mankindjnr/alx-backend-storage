fecffe67c3d66f6486437f5245740d2849e736e8
mankindjnr <kamos8646@stu.kemu.ac.ke>
2023-07-12 11:52:37 +0000

    best band ever

---
+++ 0x00-MySQL_Advanced/2-fans.sql
@@ -0,0 +1,7 @@
+-- script ranks country origin of bands
+-- import a table dump
+-- column names must be origin and nb_fans
+SELECT origin, SUM(fans) AS nb_fans
+FROM metal_bands
+GROUP BY origin
+ORDER BY nb_fans DESC;
